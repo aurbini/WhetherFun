ed6dac0ff77507b4633d83f2608ee58c5b3b0de9
Alex Urbini <aurbini92@gmail.com>
2020-01-18 15:54:52 -0500

    geocode and zomato call work

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 //Use geoCoding api 
 
 function geoCodingAPI (){
+  var street = '285 Fulton St,' ; 
+  var city = 'New York,';
+  var state = 'NY,'; 
+  var zipCode = '10007,'; 
+  var country = 'United States'; 
+
+
   var apiKey = 'b779461f362d4375b7fe76ba33e12e1f';
-  var queryUrl = `https://api.opencagedata.com/geocode/v1/json?q=${'1859 Gallo Drive, Powell, Ohio, 43065, United States'}&key=${apiKey}`;
+  var queryUrl = `https://api.opencagedata.com/geocode/v1/json?q=${street}${city}${state}${zipCode}${country}&key=${apiKey}`;
+
 
   $.ajax({
     url:  queryUrl ,
@@ -11,7 +19,7 @@ function geoCodingAPI (){
   }).then(function(response) {
     var lat = response.results[0].geometry.lat;
     var lon = response.results[0].geometry.lng;
-    //console.log(lat, lon); 
+    console.log(lat, lon); 
     zomatoCall(lat, lon); 
 
 
@@ -24,10 +32,10 @@ geoCodingAPI();
 function zomatoCall (lat, lon){
 
 var apiKey= '9be8eb8bb66ec64005c8cc43793d3c60'
-var queryURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}`
+var queryURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}&start=0&count=20`
 $.ajax({
   method: "GET",
-  url: queryURL
+  url: queryURL,
   crossDomain: true,
   dataType: "json",
   async: true,
@@ -36,62 +44,30 @@ $.ajax({
       }, success: function(data){
        console.log(data); 
       
-  })
-
-
-// }).then(function(response){
-//     console.log(response); 
-}
-// zomatoCall(); 
-
-
-// $.ajax({
-//   url: URL ,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response); 
+    }
+   })
 
 
 
 
 
 
-//Trying to find movie api by zipcode but not working 
-// var movie = 'Terminator'; 
-
-// var genre = "action"; 
-
-// var queryURL = "https://www.omdbapi.com/?t=" +  "&apikey=trilogy";
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response); 
-
-// }); 
-
-//Fandango API Key 
-
-// function fandangoCall (){
-// var apiKey = 'ar5fp4yqxxx3cekmk9v6gd4s'; 
-// var url = '/developer.fandango.com/forum/read/161673';
-// var query = 'Miami'; 
-// var queryURL = 'http/developer.fandango.com/forum/read/161673?'
-
-// $.ajax({
-//   url: `https://ee.iva-api.com/api/Fandango/movies/?Offset=0&Limit=50&Type=InTheaters&ZipCode=10028&Radius=50&subscription-Key=${apiKey}`, 
-//   method: "GET"
-// }).then(function(response) {
-//   console.log(response); 
 
 
-// }); 
-// }
-// fandangoCall()
-=======
 
 
-//alert('hello ')
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+//////--------------------------------------------------------------
