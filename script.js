bd4fab5a4f15dcc3a6c6727ee54a9f29dabb7257
Marc Erasmo <mde9259@gmail.com>
2020-01-22 14:26:02 -0500

    changed sad to relax

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Use geoCoding api 
 $(document).ready(function(){
   //API CAlls---------------------------------------------------------------------
@@ -171,13 +170,14 @@ $(document).ready(function(){
   }
   ///RElaxed 
   function sadLogic(goodWeather){
-    var sad = true; 
+    console.log(goodWeather)
+  //  var sad = true; 
     console.log('relax'); 
     if(goodWeather){
       //Restaurant when relaxed and weather is good
       geoCodingAPI(); 
     }else{
-     // when weather is bad and relaxedmovieCall();
+      getMovies();
     }
   }
 
@@ -278,7 +278,7 @@ function renderPark(parkInfo){
     }
   }
 
-})
+
 
 
 
@@ -431,3 +431,90 @@ function renderPark(parkInfo){
   //   //containerForm.innerHTML = '';
 
   // })
+
+//---------------------------------------------------------------------------------------------------//
+
+    $('#searchForm').on('submit', (e) => {
+        var searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+
+
+function getMovies(searchText) {
+//console.log(searchText);
+    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=b489d0e' + '&s=' + searchText)
+        .then((response) => {
+
+            console.log(response);
+            var movies = response.data.Search;
+            var output = '';
+            $.each(movies, (index, movie) => {
+
+                output += `
+
+                
+                    <div class = "col-md-3">
+                        <div class="well text-center">
+                            <img src="${movie.Poster}">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imdbID})" class="btn btn-primary" href="movie.html">Movie Detail</a>
+                        </div>
+                    </div>
+                `;
+            });
+            $('#movies').html(output);
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+
+function movieSelected(id) {
+    sessionStorage.setItem('imdbID', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie() {
+    var movieId = sessionStorage.getItem('imdbID');
+
+    axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=b489d0e' + '&i=' + movieId)
+        .then((response) => {
+            console.log(response);
+            var movie = response.data;
+
+            var output = `
+                <div class = "row>
+                    <div class="col-md-4">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    
+                    <div class="col-md-8>
+                        <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Genre:</strong>${movie.Genre}</li>
+                            <li class="list-group-item"><strong>Released:</strong>${movie.Released}</li>
+                            <li class="list-group-item"><strong>Rated:</strong>${movie.Rated}</li>
+                            <li class="list-group-item"><strong>IMDB Rating:</strong>${movie.imdbRating}</li>
+                            <li class="list-group-item"><strong>Director:</strong>${movie.Director}</li>
+                            <li class="list-group-item"><strong>Writer:</strong>${movie.Writer}</li>
+                            <li class="list-group-item"><strong>Actors:</strong>${movie.Actors}</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <h3>Plot</h3>
+                        ${movie.Plot}
+                        <hr>
+                        <a href ="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                        <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                    </div>
+                </div>
+            `;
+            $('#movie').html(output);
+        }).catch((err) => {
+            console.log(err);
+        });
+}
+})
